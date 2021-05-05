3ee9fc4795841f86214be8f86e907447c3455334
Divino J. Silva <divino_j_silva@hotmail.com>
2021-05-05 17:22:38 -0300

    Primeiros form

---
+++ ClientesCLT/Forms/CLTLoginForm.cs
@@ -0,0 +1,32 @@
+﻿using ClientesCLT.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientesCLT
+{
+    public partial class CLTLoginForm : Form
+    {
+        public CLTLoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void CLTLogin_Load(object sender, EventArgs e)
+        {
+
+            await Task.Delay(5000); //Espera 5 segundos
+           // Close(); // flecha o formulario
+            this.Hide(); //Oculta o formulario
+
+            var logs = new CLTinsideForm(); //stancia o novo formulario
+            logs.Show(); //Carrega o nosso novo formulario.
+        }
+    }
+}
---
+++ ClientesCLT/Forms/CLTinsideForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClientesCLT.Forms
+{
+    public partial class CLTinsideForm : Form
+    {
+        public CLTinsideForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CLTinsideForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
